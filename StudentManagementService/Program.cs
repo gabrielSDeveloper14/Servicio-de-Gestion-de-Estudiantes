c495f106fe642ed2c213a9651d7c302053e71ba5
gabrielSDeveloper14 <20241671@itla.edu.do>
2025-07-26 22:16:08 -0400

    Probando el Programs.cs

--- StudentManagementService/Program.cs
+++ StudentManagementService/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using StudentManagementService.Dtos;
+using StudentManagementService.Services;
+
+var studentService = new StudentSqlService();
+
+try
+{
+    studentService.AddStudent(new StudentDto { Name = "Gabriel", Email = "gabriel@email.com" });
+    var students = studentService.GetAllStudents();
+
+    foreach (var student in students)
+    {
+        Console.WriteLine($"{student.Id}: {student.Name} - {student.Email}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Error: " + ex.Message);
+}
